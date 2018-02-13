3f3e92cdd7a65714a46cf0b512ad6f2d650f11a9
vagrant <sowashamy@gmail.com>
2018-02-13 04:04:00 +0000

    base template added and model started. uuid wip

--- indioserver.py
+++ indioserver.py
@@ -1,12 +1,17 @@
 import sys
 import json
 import bcrypt
-from flask import Flask, render_template, jsonify, request, session, redirect
+from flask import (Flask, render_template, jsonify, request, session, redirect,
+                   flash)
 from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
 import requests
 import os
+from model import connect_to_db, db, User
 app = Flask(__name__)
-app.secret_key = "Ahahahahahha!!!!!!"
+app.secret_key = "Thanksforallthefish"
+app.jinja_env.undefined = StrictUndefined
+
 ##############################################################################
 
 
@@ -19,9 +24,44 @@ def index():
     # need way to upload images
     # way to mark image as private for each user
     # unique share link after uploading image
+    # need user log in form
+    # same url should return same image- .first()???
 
     return render_template('index.html')
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    """Login page process user info
+
+    """
+    email = request.args.get('email')
+    password = request.args.get('pword')
+    button = request.args.get('submit')
+
+    user = User.query.filter(User.email == email).first()
+
+    if user and not (bcrypt.hashpw(password.encode('utf-8'),
+                     user.password.encode('utf-8')) == user.password):
+        flash("A user with that email exists. \
+              Please login or choose a different email.")
+        return redirect('/')
+    elif user:
+        session['userid'] = user.user_id
+        return redirect('/users/' + str(user.user_id))
+
+    if button == "Register":
+        hashed_pass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        user = User(email=email, password=hashed_pass)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/users/' + str(user.user_id))
+    else:
+        flash("That email doesn't exist. \
+              Please register or choose a different email.")
+        return redirect('/')
 
 # @app.route('/image/<width>/<height>')
 # def special_display():
@@ -36,17 +76,18 @@ def index():
     #return render_template()
 
 
-# @app.route('/private')
-# def special_display():
-#     """Display private images with specific dimensions"""
+@app.route('/users/<user_id>')
+def special_display():
+    """Display user link and private images """
 
 
+    return render_template('user.html', user_id=user.user_id)
 
-#     return render_template()
+
 ##############################################################################
 if __name__ == "__main__":  # will connect to db if you run python server.py
     app.debug = True        # won't run in testy.py because it's not server.py
-    #connect_to_db(app)
+    connect_to_db(app)
     DebugToolbarExtension(app)
 
     app.run(host="0.0.0.0")
--- model.py
+++ model.py
@@ -1,31 +1,30 @@
 from flask import Flask
 import bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import text as sa_text
 
 db = SQLAlchemy()
 
 # ##############################################################################
 # Model definitions
 
+
 class User(db.Model):
     """User data to store after login"""
 
     __tablename__ = "users"
 
-    u_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    fname = db.Column(db.String(30), nullable=False)
-    lname = db.Column(db.String(30), nullable=False)
+    #u_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(UUID(as_uuid=True), primary_key=True,
+                          server_default=sa_text("uuid_generate_v4()"))
     email = db.Column(db.String(40), nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    #unique = 
 
     def __repr__(self):  # pragma: no cover
         """Useful printout of user object"""
 
-        return "<User user_id={} fname={} lname={} email={}>".format(self.u_id,
-                                                                     self.fname,
-                                                                     self.lname,
-                                                                     self.email)
+        return "<User user_id={} email={}>".format(self.user_id, self.email)
 
 ##############################################################################
 
@@ -33,9 +32,9 @@ class User(db.Model):
 def example_data():
     """Test data samples for tests.py file to use in database"""
 
-    user1 = User(fname='Brian', lname='Beaman', email="bb@bb.com",
+    user1 = User(email="bb@bb.com",
                  password=bcrypt.hashpw('bestie', bcrypt.gensalt()))
-    user2 = User(fname='Kari', lname='Baldwin', email="kb@kb.com",
+    user2 = User(email="kb@kb.com",
                  password=bcrypt.hashpw('friendly', bcrypt.gensalt()))
 
     db.session.add_all([user1, user2])
@@ -57,7 +56,8 @@ if __name__ == "__main__":
     # run this module interactively, it will leave
     # you in a state of being able to work with the database directly.
 
-    from server import app
+    from indioserver import app
 
-    connect_to_db(app)  
+    connect_to_db(app)
+    db.create_all()
     print "Connected to DB."
